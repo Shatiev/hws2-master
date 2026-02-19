83fff9745353b4e6dc3c6686d62667dcb9e175dd
Shatiev <mr_shatiev@mail.ru~>
2026-02-20 03:01:33 +0400

    hw13

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import downIcon from './icons/icon1.png'
-import upIcon from './icons/icon2.png'
-import noneIcon from './icons/icon3.png'
+import downIcon from './icons/down.png'
+import upIcon from './icons/up.png'
+import noneIcon from './icons/none.png'
 
 // добавить в проект иконки и импортировать
 
